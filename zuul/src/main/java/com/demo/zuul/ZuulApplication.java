f1c0e8aa254bf684a289181efb83cd1fa7f6fbeb
pengzg <pzgmissyou@sina.com>
2022-05-16 14:55:46 +0800

    zuul配置

--- zuul/src/main/java/com/demo/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/demo/zuul/ZuulApplication.java
@@ -2,10 +2,13 @@ package com.demo.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 
 @SpringBootApplication
-//@EnableZuulProxy
+@EnableEurekaClient
+@EnableZuulProxy
 public class ZuulApplication {
 
 	public static void main(String[] args) {
