00d3f6106b7da3b905c818e10800dca5999f9c78
pengzg <pengzg@sqkx.net>
2023-05-06 16:13:54 +0800

    fix: web02

--- feign-web02/src/main/java/com/demo/feignweb/FeignWeb02Application.java
+++ feign-web02/src/main/java/com/demo/feignweb/FeignWeb02Application.java
@@ -1,4 +1,4 @@
-package main.java.com.demo.feignweb;
+package com.demo.feignweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
